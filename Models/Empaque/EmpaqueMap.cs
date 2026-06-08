8f70ef1e6d1995ee8b661a093a52d238c86552f0
ALP300 <aitorpalomino1@gmail.com>
2026-06-07 19:38:14 -0500

    a

--- Models/Empaque/EmpaqueMap.cs
+++ Models/Empaque/EmpaqueMap.cs
@@ -13,11 +13,13 @@ namespace Proy_back_QBD.Profiles
             CreateMap<EmpaqueCreateReq, Empaque>()
             .ForMember(a => a.Id, o => o.Ignore())
             .ForMember(a => a.ModificadorId, o => o.Ignore())
-            ;
+            .ForMember(dest => dest.IdFunda, opt => opt.MapFrom(src => src.FundaId))
+            .ForMember(dest => dest.IdCaja, opt => opt.MapFrom(src => src.CajaId))
+            .ForMember(dest => dest.IdEtiqueta1, opt => opt.MapFrom(src => src.EtiquetaId1))
+            .ForMember(dest => dest.IdEtiqueta2, opt => opt.MapFrom(src => src.EtiquetaId2));
             CreateMap<EmpaqueUpdateReq, Empaque>()
             .ForMember(a => a.Id, o => o.Ignore())
-            .ForMember(a => a.CreadorId, o => o.Ignore())
-            ;
+            .ForMember(a => a.CreadorId, o => o.Ignore());
 
         }
     }
